45de68b4893c36352c5a6fd701ff4df6501be8a6
Ghazi <117175057+gzein@users.noreply.github.com>
2026-06-08 11:00:18 +0100

    File cleanup

--- geometric_MCMC/main.py
+++
@@ -1,7 +0,0 @@
-# we follow the paper "Geometric MCMC for Infinite-Dimensional Inverse Problems" by Beskos et al. (2017)
-# specifically, example 4.2
-
-# HYPERPARAMETERS
-alpha = 0.0
-s = 1.1
-sigma2 = 1
